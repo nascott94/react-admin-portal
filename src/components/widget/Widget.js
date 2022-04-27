f4d353ad8d43bf02d2ac04a7f5fdee0247119c95
nascott <nascott94@outlook.com>
2022-04-27 15:44:10 -0600

    widgets finished, starting features

--- src/components/widget/Widget.js
+++ src/components/widget/Widget.js
@@ -1,6 +1,10 @@
 import './widget.scss';
 import React from 'react';
 import PersonOutlinedIcon from '@mui/icons-material/PersonOutlined';
+import AccountBalanceWalletOutlinedIcon from '@mui/icons-material/AccountBalanceWalletOutlined';
+import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
+import MonetizationOnOutlinedIcon from '@mui/icons-material/MonetizationOnOutlined';
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
 
 const Widget = ({ type }) => {
   let data;
@@ -26,9 +30,76 @@ const Widget = ({ type }) => {
         ),
       };
       break;
+    case 'order':
+      data = {
+        title: 'ORDERS',
+        isMoney: false,
+        link: 'view all orders',
+        icon: (
+          <ShoppingCartOutlinedIcon
+            className="icon"
+            style={{
+              backgroundColor: 'rgba(218, 165, 32, 0.2)',
+              color: 'goldenrod',
+            }}
+          />
+        ),
+      };
+      break;
+    case 'earning':
+      data = {
+        title: 'EARNINGS',
+        isMoney: true,
+        link: 'view net earnings',
+        icon: (
+          <MonetizationOnOutlinedIcon
+            className="icon"
+            style={{
+              backgroundColor: 'rgba(0, 128, 0, 0.2)',
+              color: 'green',
+            }}
+          />
+        ),
+      };
+      break;
+    case 'balance':
+      data = {
+        title: 'BALANCE',
+        isMoney: true,
+        link: 'see details',
+        icon: (
+          <AccountBalanceWalletOutlinedIcon
+            className="icon"
+            style={{
+              backgroundColor: 'rgba(128, 0, 128, 0.2)',
+              color: 'purple',
+            }}
+          />
+        ),
+      };
+      break;
+    default:
+      break;
   }
 
-  return <div>Widget</div>;
+  return (
+    <div className="widget">
+      <div className="left">
+        <span className="title">{data.title}</span>
+        <span className="counter">
+          {data.isMoney && '$'} {amount}
+        </span>
+        <span className="link">{data.link}</span>
+      </div>
+      <div className="right">
+        <div className="percentage positive">
+          <KeyboardArrowUpIcon />
+          {diff} %
+        </div>
+        {data.icon}
+      </div>
+    </div>
+  );
 };
 
 export default Widget;
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -3,6 +3,7 @@ import './home.scss';
 import Sidebar from '../../components/sidebar/Sidebar';
 import Navbar from '../../components/navbar/Navbar';
 import Widget from '../../components/widget/Widget';
+import Featured from '../../components/featured/Featured';
 
 const Home = () => {
   return (
@@ -11,7 +12,13 @@ const Home = () => {
       <div className="homeContainer">
         <Navbar />
         <div className="widgets">
-          <Widget />
+          <Widget type="user" />
+          <Widget type="order" />
+          <Widget type="earning" />
+          <Widget type="balance" />
+        </div>
+        <div className="charts">
+          <Featured />
         </div>
       </div>
     </div>
